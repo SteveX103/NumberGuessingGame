e528ff70ad2cd8700a1d401b507b6ec92af5ca4c
Pratyush Biswas <ppratyushbbiswas2003@gmail.com>
2025-08-27 11:53:04 +0530

    Update NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -41,7 +41,7 @@ def main():
      while True:
          guess_game()
          again = input("\nPlay again? (y/n) [default: y]: ").strip().lower()
-         if again == "" or again == "y":   # Enter or "y" = restart
+         if again == "" or again == "y":   # Enter or "y" = restar
             continue
          else:
             print("Thanks for playing! 👋")
@@ -49,4 +49,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
